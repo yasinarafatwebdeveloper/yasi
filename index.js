2f59c1e19d5b4d8bd05983936c72dfe74ec8b75b
yasinarafatwebdeveloper <108386550+yasinarafatwebdeveloper@users.noreply.github.com>
2022-07-01 15:39:40 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+for(var i=0;i<3;i++){
+
+
+    document.querySelectorAll(".one")[i].addEventListener("click",function(){
+        var sup=this.innerHTML;
+        document.querySelector("h1").innerHTML=sup+"is clicked our super man this is too much well as";
+        
+        
+        });
+        
+      
+
+document.querySelectorAll(".one")[0].addEventListener("click",function(){
+    var sup=this.innerHTML;
+    document.querySelector("h1").innerHTML=sup+"is clicked our super like me to you either";
+    
+    
+    });
+    
+      
+
+
+}
+
